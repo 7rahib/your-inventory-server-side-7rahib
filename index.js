4cb32251528422dd74c454c2b488fe849bba97a7
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-05-02 03:22:17 +0600

    inventory find api created

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -9,6 +10,30 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5dwt7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect()
+        const inventoryCollection = client.db('yourInventory').collection('inventory')
+
+        app.get('/inventory', async (req, res) => {
+            const query = {}
+            const cursor = inventoryCollection.find(query)
+            const inventories = await cursor.toArray()
+            res.send(inventories)
+        })
+
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir)
 
 
 // Root
